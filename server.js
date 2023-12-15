2d6b361b5be075da32c14e5b2a650cd81f1f89e7
Azamat Gaparov <agaparovkg@gmail.com>
2023-12-14 20:03:34 -0800

    Latest changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
+import { readFile, writeFile } from "node:fs/promises";
 
 const server = express();
 
@@ -13,15 +14,34 @@ server.get("/", async (req, res) => {
 
 server.get("/users", async (req, res) => {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    res.status(200).end(JSON.stringify(data));
+    const data = await readFile("./data/users.json", { encoding: "utf8" });
+    const users = JSON.parse(data);
+    res.json(users);
   } catch (error) {
-    console.log("Something went wrong!", error);
+    res.json({ error: "Something went wrong!" });
   }
 });
 
-server.get("/users/:id", () => {});
+server.get("/users/:id", async (req, res) => {
+  //   const {id} = req.params
+  const id = req.params.id;
+
+  try {
+    const data = await readFile("./data/users.json", { encoding: "utf8" });
+    const users = JSON.parse(data);
+
+    const singleUser = users.find((user) => user.id === id);
+
+    // truthy value
+    if (singleUser) {
+      res.json(singleUser);
+    } else {
+      res.json({ error: "User not found" });
+    }
+  } catch (error) {
+    res.json({ error: "Something went wrong!" });
+  }
+});
 
 server.delete("/users/:id", () => {});
 
