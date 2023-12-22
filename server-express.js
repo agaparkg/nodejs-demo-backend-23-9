aabe4a515f982046cd4cb2cd5b2efe2aea63385e
Azamat Gaparov <agaparovkg@gmail.com>
2023-12-21 16:08:45 -0800

    Removed additional code added later

--- server-express.js
+++ server-express.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
 import { readFile, writeFile } from "node:fs/promises";
 
 const server = express();
@@ -42,88 +41,11 @@ server.get("/users/:id", async (req, res) => {
   }
 });
 
-server.delete("/users/:id", async (req, res) => {
-  const userId = req.params.id;
+server.delete("/users/:id", async (req, res) => {});
 
-  try {
-    // Read existing users from the file: https://nodejs.org/api/fs.html#fspromisesreadfilepath-options
-    const data = await readFile("./data/users.json", { encoding: "utf8" });
-    const users = JSON.parse(data);
+server.post("/users", async (req, res) => {});
 
-    // Find the index of the item with the specified ID
-    const index = users.findIndex((user) => user.id === userId);
-
-    // If the user is found, remove it from the array
-    if (index !== -1) {
-      users.splice(index, 1);
-
-      // Write the updated users back to the file. https://nodejs.org/api/fs.html#fspromiseswritefilefile-data-options
-      await writeFile("./data/users.json", JSON.stringify(users));
-
-      res.status(200).json({ message: "User has been deleted successfully." });
-    } else {
-      res.status(404).json({ error: "User not found" });
-    }
-  } catch (error) {
-    res.json({ error: "Something went wrong!" });
-  }
-});
-
-server.post("/users", async (req, res) => {
-  const newUser = req.body;
-
-  //   Add id to the new item
-  newUser.id = uuidv4();
-
-  try {
-    // Read existing items from the file
-    const data = await readFile("./data/users.json", {
-      encoding: "utf8",
-    });
-
-    const users = JSON.parse(data);
-
-    // Add the new item
-    users.push(newUser);
-
-    // Write the updated items back to the file
-    await writeFile("./data/users.json", JSON.stringify(users));
-
-    res.status(200).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-// Endpoint to update (patch) a user by ID
-server.patch("/users/:id", async (req, res) => {
-  const userID = req.params.id;
-  const updatedData = req.body;
-
-  try {
-    // Read existing users data from a file
-    const data = await readFile("./data/users.json", {
-      encoding: "utf8",
-    });
-    const users = JSON.parse(data);
-
-    // Find the index of the item with the specified ID
-    const index = users.findIndex((user) => user.id === userID);
-
-    // If the item is found, update it with the new data
-    if (index !== -1) {
-      users[index] = { ...users[index], ...updatedData };
-
-      // Write the updated items back to the file. https://nodejs.org/api/fs.html#fspromiseswritefilefile-data-options
-      await writeFile("./data/users.json", JSON.stringify(users));
-      res.status(200).json(users[index]);
-    } else {
-      res.status(404).json({ error: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+server.patch("/users/:id", async (req, res) => {});
 
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
--- server-fastify.js
+++ server-fastify.js
@@ -1,6 +1,5 @@
 // Import the framework and instantiate it
 import Fastify from "fastify";
-import { v4 as uuidv4 } from "uuid";
 import { readFile, writeFile } from "node:fs/promises";
 
 const fastify = Fastify({
@@ -9,7 +8,6 @@ const fastify = Fastify({
 
 const port = process.env.PORT || 8000;
 
-// Declare a route
 fastify.get("/", async function handler(request, reply) {
   return { message: "Yay! Home path is working." };
 });
@@ -49,88 +47,11 @@ fastify.get("/users/:id", async (request, reply) => {
   }
 });
 
-fastify.delete("/users/:id", async (request, reply) => {
-  const userId = request.params.id;
+fastify.delete("/users/:id", async (request, reply) => {});
 
-  try {
-    // Read existing users from the file: https://nodejs.org/api/fs.html#fspromisesreadfilepath-options
-    const data = await readFile("./data/users.json", { encoding: "utf8" });
-    const users = JSON.parse(data);
+fastify.post("/users", async (request, reply) => {});
 
-    // Find the index of the item with the specified ID
-    const index = users.findIndex((user) => user.id === userId);
-
-    // If the user is found, remove it from the array
-    if (index !== -1) {
-      users.splice(index, 1);
-
-      // Write the updated users back to the file. https://nodejs.org/api/fs.html#fspromiseswritefilefile-data-options
-      await writeFile("./data/users.json", JSON.stringify(users));
-
-      return { message: "User has been deleted successfully." };
-    } else {
-      return { error: "User not found" };
-    }
-  } catch (error) {
-    return { error: "Something went wrong!" };
-  }
-});
-
-fastify.post("/users", async (request, reply) => {
-  const newUser = request.body;
-
-  //   Add id to the new item
-  newUser.id = uuidv4();
-
-  try {
-    // Read existing items from the file
-    const data = await readFile("./data/users.json", {
-      encoding: "utf8",
-    });
-
-    const users = JSON.parse(data);
-
-    // Add the new item
-    users.push(newUser);
-
-    // Write the updated items back to the file
-    await writeFile("./data/users.json", JSON.stringify(users));
-
-    return newUser;
-  } catch (error) {
-    return { error: "Internal Server Error" };
-  }
-});
-
-// Endpoint to update (patch) a user by ID
-fastify.patch("/users/:id", async (request, reply) => {
-  const userID = request.params.id;
-  const updatedData = request.body;
-
-  try {
-    // Read existing users data from a file
-    const data = await readFile("./data/users.json", {
-      encoding: "utf8",
-    });
-    const users = JSON.parse(data);
-
-    // Find the index of the item with the specified ID
-    const index = users.findIndex((user) => user.id === userID);
-
-    // If the item is found, update it with the new data
-    if (index !== -1) {
-      users[index] = { ...users[index], ...updatedData };
-
-      // Write the updated items back to the file. https://nodejs.org/api/fs.html#fspromiseswritefilefile-data-options
-      await writeFile("./data/users.json", JSON.stringify(users));
-      return users[index];
-    } else {
-      return { error: "User not found" };
-    }
-  } catch (error) {
-    return { error: "Internal Server Error" };
-  }
-});
+fastify.patch("/users/:id", async (request, reply) => {});
 
 // Run the server!
 const runServer = async () => {
